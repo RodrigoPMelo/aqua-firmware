0594a7103f5a8ac856029a1d2612b8df08459827
RodrigoPMelo <rodrigo.pomper@hotmail.com>
2024-08-24 16:17:48 -0300

    added alimentation logic

--- aqua/aqua.ino
+++ aqua/aqua.ino
@@ -3,13 +3,11 @@
 #include <DallasTemperature.h>
 #include <Stepper.h>
 #include <WiFi.h>
-#include "FS.h"
-#include <LittleFS.h>
+#include "nvs_flash.h"
+#include "nvs.h"
 #include <Arduino.h>
 #include <DS1302.h>
 #include "mqtt_client.h"
-
-#define FORMAT_LITTLEFS_IF_FAILED true
 
 // Access Point credentials
 const char *ap_ssid = "Aqua Access Point";
@@ -30,8 +28,11 @@ esp_mqtt_client_handle_t mqtt_client;
 // Socket server
 WiFiServer server(80);
 
+// Add NVS handle
+nvs_handle_t nvs;
+
 // Turbidity Sensor
-const int turbidityPin = 34; // Define the pin for the turbidity sensor
+const int turbidityPin = 34;  // Define the pin for the turbidity sensor
 
 // Pinos do modulo de relogio
 const int CLK = 25;
@@ -39,24 +40,30 @@ const int DAT = 26;
 const int RST = 27;
 
 // DS18B20 Sensor
-const int oneWireBus = 13; // GPIO pin where the DS18B20 is connected
+const int oneWireBus = 13;  // GPIO pin where the DS18B20 is connected
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
 
 // Stepper Motor
-const int stepsPerRevolution = 500; // change this to fit the number of steps per revolution for your motor
-const int motorPin1 = 19;           // IN1 on the ULN2003 driver
-const int motorPin2 = 18;           // IN2 on the ULN2003 driver
-const int motorPin3 = 17;           // IN3 on the ULN2003 driver
-const int motorPin4 = 16;           // IN4 on the ULN2003 driver
+const int stepsPerRevolution = 500;  // change this to fit the number of steps per revolution for your motor
+const int motorPin1 = 19;            // IN1 on the ULN2003 driver
+const int motorPin2 = 18;            // IN2 on the ULN2003 driver
+const int motorPin3 = 17;            // IN3 on the ULN2003 driver
+const int motorPin4 = 16;            // IN4 on the ULN2003 driver
 Stepper myStepper(stepsPerRevolution, motorPin1, motorPin2, motorPin3, motorPin4);
 
 // LCD Display
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Adjust the I2C address if needed
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Adjust the I2C address if needed
 
 // Define o relogio
 DS1302 rtc(RST, DAT, CLK);
-int horaAtual, minutoAtual;
+int currentHour, currentMinute;
+
+//Parametros horarios de alimentacao
+int alimHour1, alimMinute1, feeded1;
+int alimHour2, alimMinute2, feeded2;
+int alimHour3, alimMinute3, feeded3;
+
 
 // Initialize variables for period tracking
 unsigned long lastMillis = 0;
@@ -70,38 +77,37 @@ bool canSendData = false;
 float turbidityValue = 0;
 float temperatureC = 0;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   // Initialize LCD
-  lcd.init();      // Inicializa o LCD 16x2
-  lcd.backlight(); // Liga a luz de fundo do LCD
+  lcd.init();       // Inicializa o LCD 16x2
+  lcd.backlight();  // Liga a luz de fundo do LCD
   lcd.print("Initializing...");
 
-  // Initialize the file system
-  if (!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED))
-  {
-    Serial.println("LittleFS Mount Failed");
-    return;
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
   }
-  Serial.println("Little FS Mounted Successfully");
-  verifyFile("/data.txt");
-  if (connectToWifi())
-  {
-    canSendData = true; // habilitates the sending of data over mqtt
-  }
-  else
-  {
-    // Create a Access Point
-    WiFi.softAP(ap_ssid);
-    Serial.print("Access Point \"");
-    Serial.print(ap_ssid);
-    Serial.println("\" started with IP: ");
-    Serial.println(WiFi.softAPIP());
+  ESP_ERROR_CHECK(ret);
 
-    // Start the socket server
-    server.begin();
-    Serial.println("Socket server started");
+  // Open NVS handle
+  ret = nvs_open("storage", NVS_READWRITE, &nvs);
+  if (ret != ESP_OK) {
+    Serial.println("Error opening NVS handle!");
+  } else {
+    Serial.println("NVS initialized successfully");
+    // Load credentials
+    loadCredentials(ssid, password, uuid);
+    loadAlimentationTime();
+    if (connectToWifi()) {
+      canSendData = true;  // Enable sending data over MQTT
+    } else {
+      WiFi.softAP(ap_ssid);
+      server.begin();
+      Serial.println("Access Point started.");
+    }
   }
 
   // Config the RTC clock
@@ -113,7 +119,7 @@ void setup()
   sensors.begin();
 
   // Initialize stepper motor
-  myStepper.setSpeed(50); // set the speed to 50 RPM
+  myStepper.setSpeed(50);  // set the speed to 50 RPM
 
   lcd.clear();
 }
@@ -122,47 +128,57 @@ int ciclo = 0;
 int alterna = 0;
 bool flipflop = false;
 
-void loop()
-{
+void loop() {
   ciclo++;
-  if (ciclo > 999)
-  {
+  if (ciclo > 999) {
     alterna++;
-    if (alterna > 5)
-    {
+    if (alterna > 5) {
       alterna = 0;
       flipflop = !flipflop;
     }
-
-    // Read turbidity sensor
-    turbidityValue = analogRead(turbidityPin);
-    // Read temperature sensor
-    sensors.requestTemperatures();
-    temperatureC = sensors.getTempCByIndex(0);
     ciclo = 0;
     // Obtem a hora atual a partir do RTC
     Time t = rtc.time();
-    horaAtual = t.hr;
-    minutoAtual = t.min;
-    if (flipflop)
-    {
+    currentHour = t.hr;
+    currentMinute = t.min;
+    if (flipflop) {
+
+      //Verifica se e o horário da primeira alimentacao
+      if (currentHour == alimHour1 && currentMinute == alimMinute1 && feeded1 == 0) {
+        feed(500);
+        feeded1 = 1;  //altera status da comida1
+      }
+
+      //Verifica se e o horário da segunda alimentacao
+      if (currentHour == alimHour2 && currentMinute == alimMinute2 && feeded2 == 0) {
+        feed(500);
+        feeded2 = 1;  //altera status da comida2
+      }
+
+      if (currentHour == alimHour3 && currentMinute == alimMinute3 && feeded3 == 0) {
+        feed(500);
+        feeded3 = 1;  //altera status da comida2
+      }
 
       // Resets the counters on midnight
-      if (horaAtual == 0 && minutoAtual == 0)
-      {
+      if (currentHour == 0 && currentMinute == 0) {
+        feeded1 = 0;
+        feeded2 = 0;
+        feeded3 = 0;
       }
 
       // alimentarPeixe(stepsPerRevolution);
-    }
-    else
-    {
+    } else {
       // Run every x seconds
-      if (canSendData)
-      {
-        
-        if (millis() - lastMillis >= 1 * 15000UL)
-        {
-          lastMillis = millis(); // get ready for the next iteration
+      if (canSendData) {
+
+        if (millis() - lastMillis >= 1 * 15000UL) {
+          lastMillis = millis();  // get ready for the next iteration
+          // Read turbidity sensor
+          turbidityValue = analogRead(turbidityPin);
+          // Read temperature sensor
+          sensors.requestTemperatures();
+          temperatureC = sensors.getTempCByIndex(0);
           lcd.clear();
           lcd.setCursor(0, 0);
           lcd.print("Turbidez: ");
@@ -173,18 +189,15 @@ void loop()
           lcd.print(" C");
           sendSensorsData(turbidityValue, temperatureC);
         }
-      }
-      else
-      {
+      } else {
         handleSocketClient();
       }
     }
   }
-} // end loop
+}  // end loop
 
 // Feed the fish
-void alimentarPeixe(int steps)
-{
+void feed(int steps) {
   delay(1000);
   // Activate the step motor
   myStepper.step(steps);
@@ -203,44 +216,35 @@ void alimentarPeixe(int steps)
 }
 
 
-void handleSocketClient()
-{
+void handleSocketClient() {
   WiFiClient client = server.available();
 
-  if (client)
-  {
+  if (client) {
     Serial.println("New Client.");
-    while (client.connected())
-    {
-      if (client.available())
-      {
+    while (client.connected()) {
+      if (client.available()) {
         String received = client.readStringUntil('\n');
         Serial.print("Received: ");
         processClientMessage(received);
-        if (ssid != "" && password != "" && uuid != "")
-        {
-          char message[256]; // Ensure the buffer is large enough to hold the concatenated string
+        if (ssid != "" && password != "" && uuid != "") {
+          char message[256];  // Ensure the buffer is large enough to hold the concatenated string
           snprintf(message, sizeof(message), "ssid: %s\r\npassword: %s\r\nuuid: %s\r\n", ssid, password, uuid.c_str());
 
-          writeFile(LittleFS, "/data.txt", message);
+          saveCredentials(ssid.c_str(), password.c_str(), uuid.c_str());
           client.println("Valid message received and saved: " + received);
-        }
-        else
-        {
+        } else {
           client.println("Invalid message received:" + received);
         }
       }
     }
     client.stop();
     Serial.println("Client Disconnected.");
-    if (connectToWifi())
-    {
-      canSendData = true; // habilitates the sending of data over mqtt
+    if (connectToWifi()) {
+      canSendData = true;  // habilitates the sending of data over mqtt
     }
   }
 }
-void processClientMessage(String message)
-{
+void processClientMessage(String message) {
   Serial.println("Received message: " + message);
 
   int firstSeparator = message.indexOf(',');
@@ -252,88 +256,112 @@ void processClientMessage(String message)
 
   Serial.printf("UUID: %s, SSID: %s, Password: %s\n", uuid.c_str(), ssid.c_str(), password.c_str());
 }
+// save to credentials to nvs flash
+void saveCredentials(const char *ssid, const char *password, const char *uuid) {
+  esp_err_t err;
 
-// Overwrite a file or creates it if it dont exists
-void writeFile(fs::FS &fs, const char *path, const char *message)
-{
-  Serial.printf("Writing file: %s\r\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("- failed to open file for writing");
-    return;
-  }
-  if (file.print(message))
-  {
-    Serial.println("- file written");
-  }
-  else
-  {
-    Serial.println("- write failed");
-  }
-  file.close();
-}
-// Read the file and get the wifi credentials
-void readCredentials(fs::FS &fs, const char *path, String &ssid, String &password, String &uuid)
-{
-  Serial.printf("Reading file: %s\r\n", path);
-
-  File file = fs.open(path);
-  if (!file || file.isDirectory())
-  {
-    Serial.println("- failed to open file for reading");
+  // Save SSID
+  err = nvs_set_str(nvs, "ssid", ssid);
+  if (err != ESP_OK) {
+    Serial.printf("Failed to write SSID to NVS: %s\n", esp_err_to_name(err));
     return;
   }
 
-  String line;
-  while (file.available())
-  {
-    line = file.readStringUntil('\n');
-    line.trim(); // Remove any leading/trailing whitespace
-    if (line.startsWith("ssid:"))
-    {
-      line.remove(0, 5); // Remove "ssid:" part
-      ssid = line;
-      ssid.trim();
-    }
-    else if (line.startsWith("password:"))
-    {
-      line.remove(0, 9); // Remove "password:" part
-      password = line;
-      password.trim();
-    }
-    else if (line.startsWith("uuid:"))
-    {
-      line.remove(0, 5);
-      uuid = line;
-      uuid.trim();
-    }
+  // Save password
+  err = nvs_set_str(nvs, "password", password);
+  if (err != ESP_OK) {
+    Serial.printf("Failed to write password to NVS: %s\n", esp_err_to_name(err));
+    return;
   }
-  Serial.printf("read file: %s, %s, %s", ssid, password, uuid.c_str());
-  file.close();
+
+  // Save UUID
+  err = nvs_set_str(nvs, "uuid", uuid);
+  if (err != ESP_OK) {
+    Serial.printf("Failed to write UUID to NVS: %s\n", esp_err_to_name(err));
+    return;
+  }
+
+  // Commit written value.
+  err = nvs_commit(nvs);
+  if (err != ESP_OK) {
+    Serial.printf("Failed to commit changes to NVS: %s\n", esp_err_to_name(err));
+  } else {
+    Serial.println("Credentials saved successfully");
+  }
 }
-void verifyFile(const char *path)
-{
-  bool fileexists = LittleFS.exists(path);
-  Serial.print(fileexists);
-  if (!fileexists)
-  {
-    Serial.println("File doesnt exist");
-    Serial.println("Creating file...");
-    // Create File and add header
-    writeFile(LittleFS, path, "");
+// Read the nvs flash and get the wifi credentials
+void loadAlimentationTime() {
+  char value[64];
+  size_t length;
+
+  // Load SSID
+  length = sizeof(value);
+  esp_err_t err = nvs_get_str(nvs, "1H", value, &length);
+  if (err == ESP_OK) {
+    processAlimentationTime(String(value), &alimHour1, &alimMinute1);
+    Serial.println("H1 loaded: " + String(value));
+  } else {
+    Serial.printf("Failed to load alimentation time 1 from NVS: %s\n", esp_err_to_name(err));
   }
-  else
-  {
-    readCredentials(LittleFS, path, ssid, password, uuid);
+
+  // Load password
+  length = sizeof(value);
+  err = nvs_get_str(nvs, "2H", value, &length);
+  if (err == ESP_OK) {
+    processAlimentationTime(String(value), &alimHour2, &alimMinute2);
+    Serial.println("H2 loaded: " + String(value));
+  } else {
+    Serial.printf("Failed to load alimentation time 2 from NVS: %s\n", esp_err_to_name(err));
+  }
+
+  // Load UUID
+  length = sizeof(value);
+  err = nvs_get_str(nvs, "3H", value, &length);
+  if (err == ESP_OK) {
+    processAlimentationTime(String(value), &alimHour3, &alimMinute3);
+    Serial.println("H3 loaded: " + String(value));
+  } else {
+    Serial.printf("Failed to load alimentation time 3 from NVS: %s\n", esp_err_to_name(err));
   }
 }
 
-bool connectToWifi()
-{
-  if (ssid.equals(""))
-  {
+void loadCredentials(String &ssid, String &password, String &uuid) {
+  char value[64];
+  size_t length;
+
+  // Load SSID
+  length = sizeof(value);
+  esp_err_t err = nvs_get_str(nvs, "ssid", value, &length);
+  if (err == ESP_OK) {
+    ssid = String(value);
+    Serial.println("SSID loaded: " + ssid);
+  } else {
+    Serial.printf("Failed to load SSID from NVS: %s\n", esp_err_to_name(err));
+  }
+
+  // Load password
+  length = sizeof(value);
+  err = nvs_get_str(nvs, "password", value, &length);
+  if (err == ESP_OK) {
+    password = String(value);
+    Serial.println("Password loaded.");
+  } else {
+    Serial.printf("Failed to load password from NVS: %s\n", esp_err_to_name(err));
+  }
+
+  // Load UUID
+  length = sizeof(value);
+  err = nvs_get_str(nvs, "uuid", value, &length);
+  if (err == ESP_OK) {
+    uuid = String(value);
+    Serial.println("UUID loaded: " + uuid);
+  } else {
+    Serial.printf("Failed to load UUID from NVS: %s\n", esp_err_to_name(err));
+  }
+}
+
+bool connectToWifi() {
+  if (ssid.equals("")) {
     return false;
   }
   ssid.trim();
@@ -342,20 +370,17 @@ bool connectToWifi()
   // Attempt to connect to the received Wi-Fi credentials
   WiFi.begin(ssid.c_str(), password.c_str());
 
-  int timeout = 10; // 10 seconds timeout
-  while (WiFi.status() != WL_CONNECTED && timeout > 0)
-  {
+  int timeout = 10;  // 10 seconds timeout
+  while (WiFi.status() != WL_CONNECTED && timeout > 0) {
     delay(1000);
     Serial.print(".");
     timeout--;
   }
 
-  if (WiFi.status() == WL_CONNECTED)
-  {
+  if (WiFi.status() == WL_CONNECTED) {
     Serial.println("\nConnected to Wi-Fi!");
     // stops the AP
-    if (WiFi.softAPdisconnect(true))
-    {
+    if (WiFi.softAPdisconnect(true)) {
       Serial.print("AP closed");
     }
     // closes the web server
@@ -363,16 +388,17 @@ bool connectToWifi()
     mqtt_app_start();
     delay(5000);
     return true;
-  }
-  else
-  {
+  } else {
     Serial.println("\nFailed to connect to Wi-Fi.");
     return false;
   }
 }
 
-void sendSensorsData(float turbidity, float temperatureC)
-{
+void cleanup() {
+  nvs_close(nvs);
+}
+
+void sendSensorsData(float turbidity, float temperatureC) {
 
   snprintf(publish_topic, sizeof(publish_topic), "aqua/devices/%s/sensors", uuid.c_str());
 
@@ -380,46 +406,49 @@ void sendSensorsData(float turbidity, float temperatureC)
   snprintf(sensorData, sizeof(sensorData), "turbidez: %f, temperature: %f", turbidity, temperatureC);
 
   int msg_id = esp_mqtt_client_publish(mqtt_client, publish_topic, sensorData, 0, 2, 0);
-
 }
 void processSubscribeData(String data) {
   Serial.printf("Received data: %s\n", data);
+  if (data.startsWith("1H")) {
+    processAlimentationTime(data, &alimHour1, &alimMinute1);
+  } else if (data.startsWith("2H")) {
+    processAlimentationTime(data, &alimHour2, &alimMinute2);
+  } else if (data.startsWith("3H")) {
+    processAlimentationTime(data, &alimHour3, &alimMinute3);
+  }
 }
 
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
-    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
+  esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
 
-    switch (event_id)
-    {
+  switch (event_id) {
     case MQTT_EVENT_CONNECTED:
-        Serial.println("MQTT_EVENT_CONNECTED");
-        {
-            String subTopic = "aqua/devices/" + uuid + "/actuators";
-            esp_mqtt_client_subscribe(mqtt_client, subTopic.c_str(), 0);
-        }
-       canSendData = true;
-        break;
+      Serial.println("MQTT_EVENT_CONNECTED");
+      {
+        String subTopic = "aqua/devices/" + uuid + "/actuators";
+        esp_mqtt_client_subscribe(mqtt_client, subTopic.c_str(), 0);
+      }
+      canSendData = true;
+      break;
 
     case MQTT_EVENT_DISCONNECTED:
-        Serial.println("MQTT_EVENT_DISCONNECTED");
-         canSendData = false;
-        break;
+      Serial.println("MQTT_EVENT_DISCONNECTED");
+      canSendData = false;
+      break;
 
     case MQTT_EVENT_DATA:
-        Serial.println("MQTT_EVENT_DATA");
-        //Serial.printf("Received topic: %.*s\n", event->topic_len, event->topic);
-        
-        processSubscribeData(event->data);
-        break;
+      Serial.println("MQTT_EVENT_DATA");
+      //Serial.printf("Received topic: %.*s\n", event->topic_len, event->topic);
+
+      processSubscribeData(event->data);
+      break;
 
     default:
-        break;
-    }
+      break;
+  }
 }
 
-void mqtt_app_start(void)
-{
+void mqtt_app_start(void) {
   esp_mqtt_client_config_t mqtt_cfg = {};
   mqtt_cfg.broker.address.uri = mqtt_server;
   mqtt_cfg.broker.address.port = mqtt_port;
@@ -430,12 +459,15 @@ void mqtt_app_start(void)
   esp_mqtt_client_start(mqtt_client);
 }
 
-void processAlimentationTime(String message, int* h, int* min) {
+void processAlimentationTime(String message, int *h, int *min) {
   int hyphenIndex = message.indexOf('-');
   int firstColonIndex = message.indexOf(':');
-
+  String prefix;
+  esp_err_t err;
   // Check if both the hyphen and colons are found
   if (hyphenIndex != -1 && firstColonIndex != -1) {
+    // Extract prefix (the part before the hyphen)
+    prefix = message.substring(0, hyphenIndex);
     // Extract hour and minute substrings
     String hourString = message.substring(hyphenIndex + 1, firstColonIndex);
     String minuteString = message.substring(firstColonIndex + 1);
@@ -443,6 +475,13 @@ void processAlimentationTime(String message, int* h, int* min) {
     // Convert substrings to integers and assign to pointers
     *h = hourString.toInt();
     *min = minuteString.toInt();
+    Serial.printf("%s, %s, %s", prefix, hourString, minuteString);
   }
-  
-  
+
+  // Save the alimentation time to nvs
+  err = nvs_set_str(nvs, prefix.c_str(), message.c_str());
+  if (err != ESP_OK) {
+    Serial.printf("Failed to write %s to NVS: %s\n", prefix, esp_err_to_name(err));
+    return;
+  }
+}
